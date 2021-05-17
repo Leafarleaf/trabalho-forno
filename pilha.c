fa341ca77e80aaffb310a0844adf302bb92834e2
Eduardo Moraes Viana <Etoox>
2021-05-17 16:38:49 -0400

    first commit

---
+++ pilha.c
@@ -0,0 +1,13 @@
+int empty() //retorna se uma pilha esta ou nao vazia.
+
+int size() //retorna o numero de elementos da pilha.
+
+int top() //retorna o indice o elemento no topo da pilha,mas sem remove-lo.
+
+int push() //insere um novo elemento no topo da pilha.
+
+int pop() //remove o elemento no topo da pilha.
+
+int swap() //troca todos os elementos de uma pilha A pelos elementos da pilha B.
+
+int clear() //remove todos os elementos da pilha,limpado-a.
