e31482b6a04ca09ba258e0fe128df7e21bf41cb1
Leafarleaf <84347841+Leafarleaf@users.noreply.github.com>
2021-05-17 16:50:10 -0400

    Update fila.c

---
+++ fila.c
@@ -0,0 +1,15 @@
+int empty() //retorna se uma fila esta ou nao vazia.
+
+int size() //retorna o numero de elementos da fila.
+
+int front() //retorna o indice do mais antigo elemento da fila,mas sem remove-lo.
+
+int pback() //retorna o indice do mais novo elemento da fila,mas sem remove-lo.
+
+int enqueue() //insere um novo elemento no topo da fila.
+
+int dequeue() //remove o elemento no topo da fila.
+
+int swap() //troca todos os elementos de uma fila A pelos elementos da fila B.
+
+int clear() //remove todos os elementos da fila,limpado-a.
